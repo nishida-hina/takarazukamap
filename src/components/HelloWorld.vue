540d6e9fe3d3aa5f311518293a66fd97c7eadbe6
hina <nishi.hina29@gmail.com>
2022-05-20 11:59:08 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div >
+    <p>ここに打ったデータ表示</p>
+    <input type="text" v-model="inputText">
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+      <li v-for="(item,index) in textData" v-bind:key="index">
+        <button v-on:click="SearchAns(item.name)" >{{ item.name }}</button>
+        </li>
+        <li >{{ profile.class }}</li>
+    </ul><br>
+
+    <button v-on:click="openImg"><svg version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 512 512" style="width: 256px; height: 256px; opacity: 1;" xml:space="preserve">
+<g>
+	<path class="st0" d="M256,224.828c-34.344,0-62.156,28.078-62.156,62.719s27.813,62.719,62.156,62.719s62.156-28.078,62.156-62.719
+		S290.344,224.828,256,224.828z" style="fill: rgb(75, 75, 75);"></path>
+	<path class="st0" d="M478.766,135.75h-58.625c-13.078,0-24.938-7.75-30.297-19.781l-17.547-39.313
+		c-5.359-12.016-17.234-19.766-30.313-19.766H170.016c-13.078,0-24.953,7.75-30.328,19.766l-17.531,39.313
+		C116.797,128,104.938,135.75,91.859,135.75H33.234C14.875,135.75,0,150.766,0,169.266v252.328c0,18.5,14.875,33.516,33.234,33.516
+		h244.25h201.281c18.344,0,33.234-15.016,33.234-33.516V169.266C512,150.766,497.109,135.75,478.766,135.75z M256,403.844
+		c-63.688,0-115.297-52.063-115.297-116.297S192.313,171.234,256,171.234s115.297,52.078,115.297,116.313
+		S319.688,403.844,256,403.844z" style="fill: rgb(75, 75, 75);"></path>
+</g>
+</svg></button>
+
+  <ul v-if="Disp">
+    <li v-for="(item, index) in WomanDatas" v-bind:key="index">
+      {{ item.class }}
+    </li>
+  </ul>
   </div>
 </template>
-
+<!-- v-if、v-forは同じタグの中に入れないというのが条件 -->
 <script>
+import datas from '../data_1.json'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      inputText: '',
+      WomanDatas: datas,
+      Disp: false,
+      disp_2:true,
+      profile:'',
+    }
+  },
+  computed: {
+    textData() {
+      if(this.inputText=='') {
+        return ''
+      } else {
+      return this.WomanDatas.filter((WomanDatas)=>{
+        return WomanDatas.name.indexOf(this.inputText) != -1 ||
+        WomanDatas.class.indexOf(this.inputText) != -1 ||
+        WomanDatas.birthplace.indexOf(this.inputText) != -1
+      })
+      }
+    }
+  },
+  methods: {
+    openImg: function(){
+      this.Disp = !this.Disp  
+    },
+    // アイコンを押したときそれがturuなら！＝でその表示の逆になる
+  //   openImg(){
+  //     // console.log('click');
+  //   return this.WomanDatas.filter(function(i) {
+  //       console.log(i.name)
+  //       return i.name
+  //   })
+  // },
+    SearchAns: function(name){
+      this.disp_2 = !this.disp_2  
+      // console.log(this.disp);
+      this.profile = this.WomanDatas.find(item => {
+        return item.name === name
+      })
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.st0{
+  fill:#b82d2d
 }
 </style>
